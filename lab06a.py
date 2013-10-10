c2ecddeecf9810ad298af7588967406285dae7cb
James M <asdfghjkl1234567890zz@gmail.com>
2013-10-09 21:00:24 -0400

    new lab06a almost done with function

--- lab06a.py
+++ lab06a.py
@@ -1,21 +1,24 @@
 def make_new_row(old_row):
     new_row=[1]
     pos=1
-    while True:
-        new_row.insert(pos,old_row[pos-1]+old_row[pos])
-        if old_row[pos]==1:
-            new_row.append(1)
-            break
-        pos+=1
-    return new_row;
+    if old_row==[]:
+        print("[1]")
+    elif old_row==[1]:
+        print("[1,1]")
+    else:
+        while True:
+            new_row.insert(pos,old_row[pos-1]+old_row[pos])
+            if old_row[pos]==1:
+                new_row.append(1)
+                break
+            pos+=1
+        return new_row;
 
 rows_in=input("Input height of Pascal's triangle...")
 rows=int(rows_in)
+old_row=[]
 for rownum in range (rows):
-    newValue=1
-    PrintingList = [newValue]
-    for iteration in range (rownum):
-        newValue = newValue * ( rownum-iteration ) * 1 / ( iteration + 1 )
-        PrintingList.append(int(newValue))
-    print(PrintingList)
+    newrow=make_new_row(old_row)
+    print(newrow)
+    old_row=newrow
 print()
