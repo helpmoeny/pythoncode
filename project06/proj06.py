6710fff73b06b6fc0208d2a7fab594584dc5f46d
James M <asdfghjkl1234567890zz@gmail.com>
2013-10-21 20:24:48 -0400

    changed project06 name to proj06, changed whole folder out, also all complete

--- project06/proj06.py
+++ project06/proj06.py
@@ -9,9 +9,9 @@ def prompt_user():
     return userinput
 
 def get_books():
+    all_list=[]
     try:
         fp=open('bestsellers.txt')
-        all_list=[]
         for line in fp:
             book=line.split('\t')
             all_list.append(book)
@@ -19,9 +19,9 @@ def get_books():
     except ValueError:
         print ("Oops!  That was no valid number.  Try again...")
     except IOError:
-        print ("Cannot open.  Try again...")
+        print ("Cannot open good bye...")
     except FileNotFoundError:
-        print ("File not found.  Try again...")
+        print ("File not found try again...")
     return all_list
 
 def print_book(book):
@@ -34,20 +34,27 @@ def display_books_by_year(library):
             begin=int(begin_str)
             end_str=input("Enter ending year: ")
             end=int(end_str)
+            if begin>end:
+                raise ValueError
             break
         except:
             print("Invalid input")
+    foundone=False
     for book in library:
         year_str=book[3]
         year_str2=year_str[-4:]
         year=int(year_str2)
         if year<=end and year>=begin:
             print_book(book)
+            foundone=True
+    if foundone==False:
+        print("Didn't find book with search")
         
 
 def display_books_by_month_year(library):
     month=input("Enter month (as a number, 1-12): ")
     t=input("Enter year: ")
+    foundone=False
     for book in library:
         date_str=book[3]
         date=date_str.split('/')
@@ -55,30 +62,44 @@ def display_books_by_month_year(library):
         year=int(date[2])
         if year==int(t) and int(month)==month_int:
             print_book(book)
+            foundone=True
+    if foundone==False:
+        print("Didn't find book with search")
 
 def search_by_author(library):
     t=input("Enter a author's name (or part of a name): ")
+    foundone=False
     for book in library:
         if t in book[1]:
             print_book(book)
+            foundone=True
+    if foundone==False:
+        print("Didn't find book with search")
 
 def search_by_title(library):
     t=input("Enter a title (or part of a title): ")
+    foundone=False
     for book in library:
         if t in book[0]:
             print_book(book)
+            foundone=True
+    if foundone==False:
+        print("Didn't find book with search")
 
 while True:
     library=get_books() #Made the library from the list returned by get_books
-    selection=prompt_user()
-    if selection=='1':
-        display_books_by_year(library)
-    if selection=='2':
-        display_books_by_month_year(library)
-    if selection=='3':
-        search_by_author(library)
-    if selection=='4':
-        search_by_title(library)
-    if selection=="Q":
-        print("Thank you and have a nice day")
+    if len(library)==0:
         break
+    else:
+        selection=prompt_user()
+        if selection=='1':
+            display_books_by_year(library)
+        if selection=='2':
+            display_books_by_month_year(library)
+        if selection=='3':
+            search_by_author(library)
+        if selection=='4':
+            search_by_title(library)
+        if selection=="Q" or selection=="q":
+            print("Thank you and have a nice day")
+            break
