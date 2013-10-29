9438cec9286439ae34129778ccb2e76d1ac5501c
James M <asdfghjkl1234567890zz@gmail.com>
2013-10-29 18:21:06 -0400

    lab09 almost done gg, checking to see if ace's accounted for

--- lab09/lab09.py
+++ lab09/lab09.py
@@ -3,43 +3,66 @@ import cards
 
 def play_war(h1, h2):
     #while any one has more than one card on hand:
-    while ????:
+    while True:
         h1,h2 = play_one(h1,h2)
         print("hand1:",h1)
         print("hand2:",h2)
-        # break the loop manually
-        if input("Keep Going: (Nn) to stop:").lower() == 'n':
+        if len(h2)==0:
+            print("Player 1 Wins")
             break
-    #if the length of h1 is greater than the length of h2, print player 1 wins
-    #if the length of h2 is greater than the length of h1, print player 2 wins
-    #if two players have the same number of cards on hand, Tie
+        elif len(h1)==0:
+            print("Player 2 Wins")
+            break
+        if input("Keep Going: (Q or q) to stop:") == 'q':
+            print("Concede")
+            break # break the loop manually
     
 def play_one(h1, h2):
     #get the top card from h1: top1
+    topcard1 = h1.pop( 0 )
     #get the top card from h2: top2
+    topcard2 = h2.pop( 0 )
 
     #get the rank of top1, if it is 1 (Ace), change to 13:  val1
+    val1=topcard1.get_rank()
+    if val1==1:
+        val1=13
     #get the rank of top2, if it is 1 (Ace), change to 13:  val2
+    val2=topcard2.get_rank()
+    if val2==1:
+        val2=13
     
     if val1 > val2:
         # h1 gets both cards
+        h1.append(topcard1)
+        h1.append(topcard2)
     elif val2 > val1:
         # h2 gets both cards
+        h2.append(topcard1)
+        h2.append(topcard2)
     else:
         #h1, h2 get their cards back
+        h1.append(topcard1)
+        h2.append(topcard2)
     return h1,h2
 
 def main():
     # Create a deck of cards
     the_deck = cards.Deck()
+
+    # Shuffle the deck
     the_deck.shuffle()
 
     # Create two player lists: hand1, hand2
-
-    # Shuffle the deck
+    hand1=[]
+    hand2=[]
 
     #while the deck is not empty:
         #deal card to the two players
+    while not the_deck.is_empty():
+        for i in range(26):
+            hand1.append( the_deck.deal() )
+            hand2.append( the_deck.deal() )
     
     # make a short deck, only use the first 5 cards to play
     hand1 = hand1[:5]
