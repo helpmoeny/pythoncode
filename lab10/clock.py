f923b7e9c51a562c6544f7101b21b98d7c53edaa
James M <asdfghjkl1234567890zz@gmail.com>
2013-11-06 21:40:59 -0500

    lab10 self making me angry, still havn't done clock.demo

--- lab10/clock.py
+++ lab10/clock.py
@@ -1,6 +1,7 @@
+import clock
 class Time:
     '''Creates Time'''
-    def __init__(self, hours = 12, minutes = 00, seconds = 00):
+    def __init__(self, hours = 24, minutes = 00, seconds = 00):
         '''Construct and initialize a refernce to the current time'''
         self._hours = hours
         self._minutes = minutes
@@ -8,26 +9,26 @@ class Time:
 
     def __repr__(self):
         '''Format for time'''
-        time=print( "%d:%02d:%02d" % (self._hours, self._minutes, self._seconds))
-        return str(time)
+        if self._minutes <10 or self._seconds <10:
+            string="Class Time: {}-0{}-0{}".format(self._hours,self._minutes,self._seconds)
+        else:
+            string="Class Time: {}-{}-{}".format(self._hours,self._minutes,self._seconds)
+        return string
 
     def __str__(self):
         '''Return's string with format “hh:mm:ss” which will be used to display a human-readable representation of time'''
-        return(self)
-    
-    def getHours(self):
-        return self._hours
+        return "{}-0{}-0{}".format(self._hours,self._minutes,self._seconds)
 
-    def getMinutes(self):
-        return self._minutes
-
-    def getSeconds(self):
-        return self._seconds
-
-    def show(self):
-        print( "%d:%02d:%02d" % (self._hours, self._minutes, self._seconds))
-
-    def setTime(self, hours = 12, minutes = 34, seconds = 2):
-        self._hours = hours
-        self._minutes = minutes
-        self._seconds = seconds
+    def from_str():
+        '''Updates Time with given string parameters'''
+        #self._time=time
+        time=input("Input new time(hh,mm,ss): ")
+        hours = int(time[0:2])
+        minutes = int(time[3:5])
+        print(time)
+        seconds = int(time[-2:])
+        if minutes <10 or seconds <10:
+            string="Class Time: {}-0{}-0{}".format(hours,minutes,seconds)
+        else:
+            string="Class Time: {}-{}-{}".format(hours,minutes,seconds)
+        print(string)
