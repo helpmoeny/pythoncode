27b9d4640a5ee635e2829a6b09c6371dc8cf4e4f
James M <asdfghjkl1234567890zz@gmail.com>
2013-11-06 22:50:56 -0500

    lab10 changed from_str, changed formatting, still not validating self

--- lab10/clock.py
+++ lab10/clock.py
@@ -2,31 +2,24 @@ import clock
 class Time(object):
     '''Creates Time'''
     def __init__(self, hours = 24, minutes = 00, seconds = 00):
-        '''Construct and initialize a refernce to the current time'''
+        """ Construct the Time using three integers. """
         self._hours = hours
         self._minutes = minutes
         self._seconds = seconds
 
     def __repr__(self):
-        '''Format for time'''
-        if self._minutes <10 or self._seconds <10:
-            string="Class Time: {}:0{}:0{}".format(self._hours,self._minutes,self._seconds)
-        else:
-            string="Class Time: {}:{}:{}".format(self._hours,self._minutes,self._seconds)
+        """ Return a string as the formal representation a Time. """
+        string="Class Time: {:02d}:{:02d}:{:02d}".format(self._hours,self._minutes,self._seconds)
         return string
 
     def __str__(self):
-        '''Return's string with format “hh:mm:ss” which will be used to display a human-readable representation of time'''
-        return "{}:0{}:0{}".format(self._hours,self._minutes,self._seconds)
+        """ Return a string (mm/dd/yyyy) to represent a Time. """
+        return "{:02d}:{:02d}:{:02d}".format(self._hours,self._minutes,self._seconds)
 
     def from_str(self,time_str):
         '''Updates Time with given string parameters'''
-        self._hours = int(time_str[0:2])
-        self._minutes = int(time_str[3:5])
+        hours, minutes, seconds = [ int( n ) for n in time_str.split( ':' )]
+        self._hours = hours
+        self._minutes = minutes
+        self._seconds = seconds
         print(time_str)
-        self._seconds = int(time_str[-2:])
-        if self._minutes <10 or self._seconds <10:
-            string="Class Time: {}:0{}:0{}".format(self._hours,self._minutes,self._seconds)
-        else:
-            string="Class Time: {}:{}:{}".format(self._hours,self._minutes,self._seconds)
-        print(string)
