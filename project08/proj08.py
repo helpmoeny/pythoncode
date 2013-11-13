030b4343680d96dec6c6e6a5c4d132783e79e416
James M <asdfghjkl1234567890zz@gmail.com>
2013-11-13 00:17:19 -0500

    project08 fixed shuffle, all done

--- project08/proj08.py
+++ project08/proj08.py
@@ -92,30 +92,31 @@ def move_to_tableau(tableau,cell,t_col,c_col):
     t_col=t_col-1
     c_col=c_col-1
     
-    if len(cell[c_col])!=0:
-        call_card=call[c_col].pop()
-        call_card_rank=call_card.get_rank()
-        call_card_suite=call_card.get_suit()
+    if len(cell[c_col])!=0:#checks to see if the cell's list at current column is empty
+        cell_card=cell[c_col].pop()
+        cell_card_rank=cell_card.get_rank()
+        cell_card_suite=cell_card.get_suit()
 
-        if len(tableau[t_col])==0:
-            tableau[t_col].append(call_card)
+        if len(tableau[t_col])==0:#checks to see if tableau's selected column is empty
+            tableau[t_col].append(cell_card)
+            valid=True
         else:
-            tableau_card=tableau[t_col][-1]
+            tableau_card=tableau[t_col][-1]#
             tableau_rank=tableau_card.get_rank()
             tableau_suit=tableau_card.get_suit()
 
             if tableau_rank==call_card_rank+1: #Iterating through rank
-                if (tableau_suit==1 or tableau_suit==4) and (call_card_suite==2 or call_card_suite==3):
-                    tableau[t_col].append(call_card)
+                if (tableau_suit==1 or tableau_suit==4) and (cell_card_suite==2 or cell_card_suite==3):
+                    tableau[t_col].append(cell_card)
                     valid=True
-                elif (tableau_suit==2 or tableau_suit==3) and (call_card_suite==1 or call_card_suite==4):
-                    tableau[t_col].append(call_card)
+                elif (tableau_suit==2 or tableau_suit==3) and (cell_card_suite==1 or cell_card_suite==4):
+                    tableau[t_col].append(cell_card)
                     valid=True
                 else:
-                    tableau[t_col].append(call_card)
+                    tableau[t_col].append(cell_card)
             else:
-                tableau[t_col].append(call_card)
-        return valid
+                tableau[t_col].append(cell_card)
+    return valid
 
 
 def is_winner(foundation):
@@ -145,12 +146,12 @@ def move_in_tableau(tableau,t_col_source,t_col_dest):
     t_col_source=t_col_source-1
     t_col_dest=t_col_dest-1
     
-    if len(tableau[t_col_source])!=0:
+    if len(tableau[t_col_source])!=0:#checks if tableau's first/start column's list is empty
         tableau_card1=tableau[t_col_source].pop()
         tableau_rank1=tableau_card1.get_rank()
         tableau_suit1=tableau_card1.get_suit()
 
-        if len(tableau[t_col_dest])==0:
+        if len(tableau[t_col_dest])==0:#checks if tableau's second/end column's list is empty
             tableau[t_col_dest].append(tableau_card1)
         else:
             tableau_card2=tableau[t_col_dest][-1]
@@ -295,13 +296,13 @@ def play():
                     if t_col<1 or t_col>8 or f_col<1 or f_col>4:
                         valid=False
                     else:
-                        valid=move_to_foundation(tableau,foundation,t_col,f_col)
+                        valid=move_to_foundation(tableau,foundation,t_col,f_col)#call function and set to valid
                     if valid==False:
                         print("illegal move")
                 elif r == 't2t':
                     t_col_source=start
                     t_col_dest=end
-                    if t_col_source<1 or t_col_source>8 or t_col_dest<1 or t_col_dest>8:
+                    if t_col_source<1 or t_col_source>8 or t_col_dest<1 or t_col_dest>8:#validates input parameters
                         valid=False
                     else:
                         valid=move_in_tableau(tableau,t_col_source,t_col_dest)
@@ -317,9 +318,9 @@ def play():
                     if valid==False:
                         print("illegal move")
                 elif r == 'c2t':
-                    t_col=start
-                    c_col=end
-                    if t_col<1 or t_col>8 or c_col<1 or c_col>4:
+                    c_col=start
+                    t_col=end
+                    if c_col<1 or c_col>4 or t_col<1 or t_col>8:
                         valid=False
                     else:
                         valid=move_to_tableau(tableau,cell,t_col,c_col)
